aef983c69a3405885f160bc606a5dfa4003a6092
alexis fredriksen <alexis-fredriksen@semper-co.fr>
2025-03-12 09:37:50 +0100

    Initial commit - Exo 1 avec Jest

---
+++ utils.js
@@ -0,0 +1,15 @@
+function somme(a, b) {
+  return a + b;
+}
+
+function estPair(n) {
+  return n % 2 === 0;
+}
+
+function factorielle(n) {
+  if (n < 0) throw new Error("Nombre négatif interdit");
+  if (n === 0) return 1;
+  return n * factorielle(n - 1);
+}
+
+module.exports = { somme, estPair, factorielle };
---
+++ utils.test.js
@@ -0,0 +1,25 @@
+const { somme, estPair, factorielle } = require("./utils");
+
+test("somme de 2 et 3 doit être 5", () => {
+  expect(somme(2, 3)).toBe(5);
+});
+
+test("estPair(4) doit retourner true", () => {
+  expect(estPair(4)).toBe(true);
+});
+
+test("estPair(5) doit retourner false", () => {
+  expect(estPair(5)).toBe(false);
+});
+
+test("factorielle de 5 doit être 120", () => {
+  expect(factorielle(5)).toBe(120);
+});
+
+test("factorielle de 0 doit être 1", () => {
+  expect(factorielle(0)).toBe(1);
+});
+
+test("factorielle d'un nombre négatif doit lever une erreur", () => {
+  expect(() => factorielle(-1)).toThrow("Nombre négatif interdit");
+});
